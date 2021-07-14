d00b82fc3cabfa7ebe62d31bd9406b014aa06974
SavageMessiah <cardboard42@gmail.com>
2021-07-13 20:00:28 -0400

    advent 2015 day 5 part 2

--- advent/2015/day5/src/main.rs
+++ advent/2015/day5/src/main.rs
@@ -53,10 +53,56 @@ fn nice_test() {
     assert!(!nice("dvszwmarrgswjxmb"));
 }
 
+fn split_repeat(s: &str) -> bool {
+    let chars = s.chars().collect::<Vec<char>>();
+    for triple in chars.windows(3) {
+        if triple[0] == triple[2] {
+            return true;
+        }
+    }
+    false
+}
+
+fn separate_pairs(s: &str) -> bool {
+    let chars = s.chars().collect::<Vec<char>>();
+    for (i, pair) in chars.windows(2).enumerate() {
+        for new_pair in chars[i+2..].windows(2) {
+            if pair == new_pair {
+                return true;
+            }
+        }
+    }
+    false
+}
+
+fn nice2(s: &str) -> bool {
+    split_repeat(s) && separate_pairs(s)
+}
+
+#[test]
+fn nice2_test() {
+    assert!(split_repeat("xyx"));
+    assert!(split_repeat("abcdefeghi"));
+    assert!(split_repeat("aaa"));
+
+    assert!(separate_pairs("aabcdefgaa"));
+    assert!(separate_pairs("xyxy"));
+    assert!(separate_pairs("xyaxy"));
+    assert!(!separate_pairs("aaa"));
+
+    assert!(nice2("qjhvhtzxzqqjkmpb"));
+    assert!(nice2("xxyxx"));
+    assert!(!nice2("uurcxstgmygtbstg"));
+    assert!(!nice2("ieodomkazucvgmuy"));
+}
+
 fn main() {
     let input = include_str!("input.txt");
     let nice = input.lines().
         filter(|l| nice(l)).
         count();
-    println!("Nice: {}", nice);
+    let nice2 = input.lines().
+        filter(|l| nice2(l)).
+        count();
+    println!("Nice: {} Nice 2: {}", nice, nice2);
 }
