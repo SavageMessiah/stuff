98a140740aba64fb1ad448427c78f538e527c033
SavageMessiah <cardboard42@gmail.com>
2021-07-26 22:05:32 -0400

    advent 2015 day 8 part 2, inline pull

--- advent/2015/day8/src/main.rs
+++ advent/2015/day8/src/main.rs
@@ -4,61 +4,38 @@ struct Counts {
     mem: u32,
 }
 
-//bah, wish this could be type State = fn(char, &mut Counts) -> State
-struct State(fn(char, &mut Counts) -> State);
-
-fn escape(c: char, counts: &mut Counts) -> State {
-    match c {
-        'x' => {
-            counts.encoded += 1;
-            counts.lit += 1;
-            return State(|_, counts| { //first hex
-                counts.encoded += 1;
-                counts.lit += 1;
-                return State(|_, counts| { //second hex
-                    counts.encoded += 1;
-                    counts.lit += 1;
-                    counts.mem += 1;
-                    return State(normal);
-                })
-            })
-        },
-        _ => {
-            counts.encoded += 2; // \_
-            counts.lit += 1;
-            counts.mem += 1;
-            return State(normal);
-        },
-    }
-}
-
-fn normal(c: char, counts: &mut Counts) -> State {
-    match c {
-        '"' => {
-            counts.encoded += 3; // "\" or \""
-            counts.lit += 1;
-            return State(normal);
-        },
-        '\\' => {
-            counts.encoded += 2; // \\
-            counts.lit += 1;
-            return State(escape);
-        },
-        _ => {
-            counts.encoded += 1;
-            counts.lit += 1;
-            counts.mem += 1;
-            return State(normal);
-        },
-    }
-}
-
 fn main() {
     let mut counts = Counts{encoded: 0, lit: 0, mem: 0};
     for line in include_str!("input.txt").lines() {
-        let mut s = State(normal);
-        for c in line.chars() {
-            s = s.0(c, &mut counts);
+        let mut iter = line.chars();
+        while let Some(c) = iter.next() {
+            counts.lit += 1;
+            match c {
+                '"' => {
+                    counts.encoded += 3; // "\" or \""
+                },
+                '\\' => {
+                    counts.mem += 1;
+                    counts.encoded += 2; // \\
+                    match iter.next() {
+                        Some('x') => {
+                            counts.encoded += 3; // xab
+                            counts.lit += 3;
+                            iter.next();
+                            iter.next();
+                        },
+                        Some(_) => {
+                            counts.encoded += 2; // \"
+                            counts.lit += 1;
+                        },
+                        None => panic!("bad input")
+                    }
+                },
+                _ => {
+                    counts.encoded += 1;
+                    counts.mem += 1;
+                }
+            }
         }
     }
 
