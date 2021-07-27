e056f865103dba17eb5cd0d8393e1f9abebbeb75
SavageMessiah <cardboard42@gmail.com>
2021-07-26 21:50:23 -0400

    advent 2015 day 8 part 2

--- advent/2015/day8/src/main.rs
+++ advent/2015/day8/src/main.rs
@@ -1,17 +1,22 @@
 struct Counts {
+    encoded: u32,
     lit: u32,
     mem: u32,
 }
 
+//bah, wish this could be type State = fn(char, &mut Counts) -> State
 struct State(fn(char, &mut Counts) -> State);
 
 fn escape(c: char, counts: &mut Counts) -> State {
     match c {
         'x' => {
+            counts.encoded += 1;
             counts.lit += 1;
             return State(|_, counts| { //first hex
+                counts.encoded += 1;
                 counts.lit += 1;
                 return State(|_, counts| { //second hex
+                    counts.encoded += 1;
                     counts.lit += 1;
                     counts.mem += 1;
                     return State(normal);
@@ -19,6 +24,7 @@ fn escape(c: char, counts: &mut Counts) -> State {
             })
         },
         _ => {
+            counts.encoded += 2; // \_
             counts.lit += 1;
             counts.mem += 1;
             return State(normal);
@@ -29,14 +35,17 @@ fn escape(c: char, counts: &mut Counts) -> State {
 fn normal(c: char, counts: &mut Counts) -> State {
     match c {
         '"' => {
+            counts.encoded += 3; // "\" or \""
             counts.lit += 1;
             return State(normal);
         },
         '\\' => {
+            counts.encoded += 2; // \\
             counts.lit += 1;
             return State(escape);
         },
         _ => {
+            counts.encoded += 1;
             counts.lit += 1;
             counts.mem += 1;
             return State(normal);
@@ -45,12 +54,14 @@ fn normal(c: char, counts: &mut Counts) -> State {
 }
 
 fn main() {
-    let mut counts = Counts{lit: 0, mem: 0};
+    let mut counts = Counts{encoded: 0, lit: 0, mem: 0};
     for line in include_str!("input.txt").lines() {
         let mut s = State(normal);
         for c in line.chars() {
             s = s.0(c, &mut counts);
         }
     }
+
     println!("{} - {} = {}", counts.lit, counts.mem, counts.lit - counts.mem);
+    println!("{} - {} = {}", counts.encoded, counts.lit, counts.encoded - counts.lit);
 }
