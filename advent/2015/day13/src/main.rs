eb0fd7abd8bc60eacdbadf2a885ec35bc12fa950
SavageMessiah <cardboard42@gmail.com>
2021-08-04 22:23:49 -0400

    advent 2015 day 13 part 1

---
+++ advent/2015/day13/src/main.rs
@@ -0,0 +1,60 @@
+use anyhow::{anyhow, Result};
+use itertools::Itertools;
+use lazy_static::lazy_static;
+use regex::Regex;
+use std::collections::{HashMap, HashSet};
+
+struct Rule<'a> {
+    target: &'a str,
+    amount: i32,
+    other: &'a str,
+}
+
+fn parse_line<'a>(l: &'a str) -> Result<Rule<'a>> {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"(?P<target>\w+) would (?P<sign>\w+) (?P<amount>\d+) happiness units by sitting next to (?P<other>\w+)\.").unwrap();
+    }
+
+    let caps = RE.captures(l).ok_or(anyhow!("no match"))?;
+    let sign: i32 = match &caps["sign"] {
+        "gain" => Ok(1),
+        "lose" => Ok(-1),
+        _ => Err(anyhow!("bad sign {}", &caps["sign"]))
+    }?;
+    let amount = &caps["amount"].parse::<i32>()?;
+
+
+    Ok(Rule {
+        target: caps.name("target").unwrap().as_str(),
+        amount: amount * sign,
+        other: caps.name("other").unwrap().as_str(),
+    })
+}
+
+fn happiness<'a>(rules: &HashMap<(&'a str, &'a str), i32>, peeps: &[&'a str]) -> i32 {
+    peeps.iter().copied().cycle().tuple_windows::<(&'a str, &'a str)>().take(peeps.len()).map(|p| {
+        rules.get(&p).unwrap() + rules.get(&(p.1, p.0)).unwrap()
+    }).sum()
+}
+
+fn main() -> Result<()> {
+    let rules = include_str!("input.txt").lines().map(parse_line).collect::<Result<Vec<_>>>()?;
+    let people = rules.iter().flat_map(|r| vec![r.target, r.other]).collect::<HashSet<_>>();
+    let mut combined = HashMap::new();
+    for Rule {target, amount, other} in &rules {
+        combined.insert((*target, *other), *amount);
+    }
+
+    println!("perms: {}", people.iter().permutations(people.len()).count());
+    println!("people: {:?}", people);
+    println!("one: {:?}", people.iter().copied().cycle().tuple_windows::<(&str, &str)>().take(people.len()).collect::<Vec<_>>());
+
+    let solution = people.iter().copied().permutations(people.len()).map(move |p| {
+        let happiness = happiness(&combined, &p);
+      (p, happiness)
+    }).max_by_key(|(_, h)| *h);
+
+    println!("solution: {:?}", solution);
+
+    Ok(())
+}
