cd2f5286239773f9f727a0f56ccbf17bf5cc2e81
SavageMessiah <cardboard42@gmail.com>
2021-08-09 19:17:05 -0400

    advent 2015 day 14 part 2

--- advent/2015/day14/src/main.rs
+++ advent/2015/day14/src/main.rs
@@ -21,6 +21,7 @@ struct ReindeerSim<'a> {
     def: &'a ReindeerDef,
     remaining: u32,
     flown: u32,
+    points: u32,
     state: State,
 }
 
@@ -41,27 +42,47 @@ fn parse(s: &str) -> Result<Vec<ReindeerDef>> {
     }).collect()
 }
 
-fn sim(defs: &Vec<ReindeerDef>, time: u32) -> Vec<ReindeerSim> {
-    let mut state = defs.iter().map(|def| ReindeerSim {def, remaining: def.flight_time, flown: 0, state: State::Flying}).collect::<Vec<_>>();
-    for _ in 0..time {
-        for reindeer in &mut state {
-            reindeer.remaining -= 1;
-            match reindeer.state {
-                State::Flying => {
-                    reindeer.flown += reindeer.def.speed;
-                    if reindeer.remaining == 0 {
-                        reindeer.state = State::Resting;
-                        reindeer.remaining = reindeer.def.rest_time;
-                    }
-                },
-                State::Resting => {
-                    if reindeer.remaining == 0 {
-                        reindeer.state = State::Flying;
-                        reindeer.remaining = reindeer.def.flight_time;
-                    }
+fn advance(state: &mut Vec<ReindeerSim>) {
+    for reindeer in state.iter_mut() {
+        reindeer.remaining -= 1;
+        match reindeer.state {
+            State::Flying => {
+                reindeer.flown += reindeer.def.speed;
+                if reindeer.remaining == 0 {
+                    reindeer.state = State::Resting;
+                    reindeer.remaining = reindeer.def.rest_time;
+                }
+            },
+            State::Resting => {
+                if reindeer.remaining == 0 {
+                    reindeer.state = State::Flying;
+                    reindeer.remaining = reindeer.def.flight_time;
                 }
             }
         }
+    }
+}
+
+fn award_points(state: &mut Vec<ReindeerSim>) {
+    let lead_distance = state.iter().map(|r| r.flown).max().unwrap();
+
+    state.iter_mut().filter(|r| r.flown == lead_distance).for_each(|leader| {
+        leader.points += 1;
+    });
+}
+
+fn sim(defs: &Vec<ReindeerDef>, time: u32) -> Vec<ReindeerSim> {
+    let mut state = defs.iter().map(|def| ReindeerSim {
+        def,
+        remaining:
+        def.flight_time,
+        flown: 0,
+        points: 0,
+        state: State::Flying}).collect::<Vec<_>>();
+
+    for _ in 0..time {
+        advance(&mut state);
+        award_points(&mut state);
     }
     state
 }
@@ -70,9 +91,9 @@ fn sim(defs: &Vec<ReindeerDef>, time: u32) -> Vec<ReindeerSim> {
 fn main() -> Result<()> {
     let defs = parse(include_str!("input.txt"))?;
     let res = sim(&defs, 2503);
-    let winner = res.iter().max_by_key(|reindeer| reindeer.flown).unwrap();
+    let winner = res.iter().max_by_key(|reindeer| reindeer.points).unwrap();
 
-    println!("Winner is {} at {} km", winner.def.name, winner.flown);
+    println!("Winner is {} at {} points", winner.def.name, winner.points);
 
     Ok(())
 }
