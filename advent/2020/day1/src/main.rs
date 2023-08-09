fc7ab398830d084cbde93b13f56b7eb933bc8bbd
SavageMessiah <cardboard42@gmail.com>
2023-08-08 23:01:27 -0400

    advent 2020 day 1: just use u32 you doofus

--- advent/2020/day1/src/main.rs
+++ advent/2020/day1/src/main.rs
@@ -2,11 +2,11 @@ use itertools::Itertools;
 
 fn main() -> anyhow::Result<()> {
     let input = std::fs::read_to_string("input.txt")?;
-    let rows = input.lines().map(|l| l.parse::<u16>()).collect::<Result<Vec<_>, _>>()?;
+    let rows = input.lines().map(|l| l.parse::<u32>()).collect::<Result<Vec<_>, _>>()?;
 
-    let answer = rows.into_iter().combinations(3).find(|n| n.iter().sum::<u16>() == 2020 ).expect("no match");
+    let answer = rows.into_iter().combinations(3).find(|n| n.iter().sum::<u32>() == 2020 ).expect("no match");
 
-    println!("answer {:?} {}", answer, answer.iter().map(Clone::clone).map(u32::from).product::<u32>());
+    println!("answer {:?} {}", answer, answer.iter().product::<u32>());
 
     Ok(())
 }
