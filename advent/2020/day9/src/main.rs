7672f75b7ddf342ec4b54d5350e9af40af70c9ce
SavageMessiah <cardboard42@gmail.com>
2023-08-16 16:52:52 -0400

    advent 2020 day 9 part 2

--- advent/2020/day9/src/main.rs
+++ advent/2020/day9/src/main.rs
@@ -1,6 +1,6 @@
 use std::num::ParseIntError;
 use itertools::Itertools;
-use anyhow::Result;
+use anyhow::{Result, anyhow};
 
 fn valid(window: &[u64], val: u64) -> bool {
     window.iter().tuple_combinations().any(|(a, b)| a + b == val)
@@ -9,8 +9,18 @@ fn valid(window: &[u64], val: u64) -> bool {
 fn main() -> Result<()> {
     let input = std::fs::read_to_string("input.txt")?;
     let ns = input.lines().map(|l| l.parse()).collect::<Result<Vec<u64>, ParseIntError>>()?;
-    let answer = ns.windows(25).zip(ns.iter().skip(25)).find(|(win, v)| !valid(win, **v)).unwrap().1;
+    let invalid = ns.windows(25).zip(ns.iter().skip(25)).find(|(win, v)| !valid(win, **v)).unwrap().1;
+    for start in 0..ns.len() {
+        for end in start..=ns.len() {
+            let sub = &ns[start..end];
+            if sub.iter().sum::<u64>() != *invalid {
+                continue;
+            }
+            let answer = sub.iter().max().unwrap() + sub.iter().min().unwrap();
+            println!("answer: {}", answer);
+            return Ok(());
+        }
+    }
 
-    println!("answer: {}", answer);
-    Ok(())
+    Err(anyhow!("no answer?"))
 }
