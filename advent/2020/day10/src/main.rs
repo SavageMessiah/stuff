e906e7e6ce9581b7e7c9ac2ef20db42c81b5f71f
SavageMessiah <cardboard42@gmail.com>
2023-08-16 19:34:32 -0400

    advent 2020 day 10 part 2

--- advent/2020/day10/src/main.rs
+++ advent/2020/day10/src/main.rs
@@ -1,7 +1,28 @@
-use std::num::ParseIntError;
-use itertools::Itertools;
+use std::{cmp::min, num::ParseIntError, collections::HashMap};
 use anyhow::Result;
 
+fn count_chains(rest: &[u32], memo: &mut HashMap<u32, u64>) -> u64 {
+    if rest.len() == 1 {
+        return 1;
+    }
+
+    let mut total = 0;
+    let this = rest[0];
+    for i in 1..min(4, rest.len()) {
+        let next = rest[i];
+        if next - this > 3 {
+            break;
+        }
+        total += if let Some(n) = memo.get(&next) {
+            *n
+        } else {
+            let n = count_chains(&rest[i..], memo);
+            memo.insert(next, n);
+            n
+        }
+    }
+    total
+}
 
 fn main() -> Result<()> {
     let input = std::fs::read_to_string("input.txt")?;
@@ -10,8 +31,8 @@ fn main() -> Result<()> {
     adapters.push(0);
     adapters.push(max);
     adapters.sort();
-    let counts = adapters.iter().tuple_windows().map(|(a, b)| b - a).counts();
-    let answer = counts[&1] * counts[&3];
+    let mut memo = HashMap::new();
+    let answer = count_chains(&adapters, &mut memo);
 
     println!("answer: {:?}", answer);
 
