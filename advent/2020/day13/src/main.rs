6d76cfdc27e82edfc7b64287e5fc2d0c41708215
SavageMessiah <cardboard42@gmail.com>
2023-08-18 16:23:39 -0400

    advent 2020 day 13 part 2

--- advent/2020/day13/src/main.rs
+++ advent/2020/day13/src/main.rs
@@ -1,20 +1,67 @@
 use anyhow::{anyhow, Result};
 
+fn parse_busses(s: &str) -> Vec<(u64, u64)> {
+    s.split(',')
+     .map(|b| b.parse::<u64>().ok())
+     .enumerate()
+     .filter_map(|(i, b)| {
+         match b {
+             None => None,
+             Some(b) => Some((i as u64, b))
+         }
+     }).collect::<Vec<_>>()
+}
+
+fn find_timestamp(busses: &Vec<(u64, u64)>) -> Option<u64> {
+    let first = busses[0].1;
+    'outer: for n in 0.. {
+        let time = first * n;
+        for (offset, b) in busses.iter().skip(1) {
+            if (time + *offset) % b != 0 {
+                continue 'outer;
+            }
+        }
+
+        return Some(time);
+    }
+    None
+}
+
+fn find_timestamp_not_shit(busses: &Vec<(u64, u64)>) -> Option<u64> {
+    let mut crt = busses.iter().copied().map(|(i, b)| (if i == 0 { 0 } else { b - (i % b) }, b)).collect::<Vec<_>>();
+    crt.sort_by_key(|(_, n)| *n);
+    crt.reverse();
+    println!("{:?}", crt);
+    let (mut n, mut modulo) = crt[0];
+    for (rem, m) in crt.iter().skip(1) {
+        while n % m != *rem {
+            n += modulo;
+        }
+        modulo *= m;
+    }
+    Some(n)
+}
+
+#[test]
+fn test_find_timestamp() {
+    let examples = [("7,13,x,x,59,x,31,19",1068781),
+                    ("17,x,13,19",3417),
+                    ("67,7,59,61",754018),
+                    ("67,x,7,59,61",779210),
+                    ("67,7,x,59,61",1261476),
+                    ("1789,37,47,1889",1202161486)];
+    for (s, ex) in examples {
+        let busses = parse_busses(s);
+        let t = find_timestamp_not_shit(&busses);
+        assert_eq!(t, Some(ex));
+    }
+}
 
 fn main() -> Result<()> {
     let input = std::fs::read_to_string("input.txt")?;
-    let mut lines = input.lines();
-    let earliest_time = lines.next().ok_or(anyhow!("empty input"))?.parse::<u32>().unwrap();
-    let busses = lines.next().ok_or(anyhow!("missing second line"))?.split(',').filter_map(|b| b.parse::<u32>().ok()).collect::<Vec<_>>();
-    let (next_bus, time) = busses.iter().map(|b| {
-        let mut mult = earliest_time / b;
-        if earliest_time % b != 0 {
-            mult += 1;
-        }
-        (*b, mult * b)
-    }).min_by_key(|p| p.1).ok_or(anyhow!("no busses"))?;
-    let answer = next_bus * (time - earliest_time);
-
+    let busses = parse_busses(input.lines().nth(1).ok_or(anyhow!("missing second line"))?);
+    println!("{:?}", busses);
+    let answer = find_timestamp_not_shit(&busses);
     println!("answer: {:?}", answer);
 
     Ok(())
