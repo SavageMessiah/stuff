a4bdee6c2163d3bb18fdc4dfb9c0b948133e906f
SavageMessiah <cardboard42@gmail.com>
2023-08-17 20:53:10 -0400

    advent 2020 day 12 part 2

--- advent/2020/day12/src/main.rs
+++ advent/2020/day12/src/main.rs
@@ -1,4 +1,4 @@
-use std::{str::FromStr, f64::consts::PI};
+use std::{str::FromStr};
 
 use anyhow::{Result, anyhow};
 
@@ -39,43 +39,81 @@ impl FromStr for Instr {
     }
 }
 
+#[derive(Clone, Copy, Debug, Default)]
+struct Point {
+    x: i64,
+    y: i64
+}
+
+impl Point {
+    fn to_polar(&self) -> (f64, f64) {
+        let fx = self.x as f64;
+        let fy = self.y as f64;
+        (fx.hypot(fy), fy.atan2(fx))
+    }
+
+    fn rotate(&self, deg: i64) -> Point {
+        let (r, rads) = self.to_polar();
+        let rads = (rads.to_degrees() + deg as f64).to_radians();
+        let (sin, cos) = rads.sin_cos();
+        Point {
+            x: (r * cos).round() as i64,
+            y: (r * sin).round() as i64
+        }
+    }
+}
 
 #[derive(Clone, Copy, Debug, Default)]
 struct Ship {
-    x: i64,
-    y: i64,
-    dir: i64
+    pos: Point,
+    waypoint: Point,
 }
 
 impl Ship {
     fn go(&mut self, instr: Instr) {
         match instr.op {
-            Op::N => self.y -= instr.val,
-            Op::S => self.y += instr.val,
-            Op::E => self.x += instr.val,
-            Op::W => self.x -= instr.val,
-            Op::L => {self.dir -= instr.val; self.dir %= 360},
-            Op::R => {self.dir += instr.val; self.dir %= 360},
+            Op::N => self.waypoint.y += instr.val,
+            Op::S => self.waypoint.y -= instr.val,
+            Op::E => self.waypoint.x += instr.val,
+            Op::W => self.waypoint.x -= instr.val,
+            Op::L => self.waypoint = self.waypoint.rotate(instr.val),
+            Op::R => self.waypoint = self.waypoint.rotate(-instr.val),
             Op::F => {
-                let a = self.dir as f64 * (PI / 180.0) ;
-                let d = instr.val as f64;
-                let x = (a.cos() * d).round() as i64;
-                let y = (a.sin() * d).round() as i64;
-                self.x += x;
-                self.y += y;
+                self.pos.x += self.waypoint.x * instr.val;
+                self.pos.y += self.waypoint.y * instr.val;
             },
         }
     }
 
     fn manhattan_distance(&self) -> i64 {
-        self.x.abs() + self.y.abs()
+        self.pos.x.abs() + self.pos.y.abs()
     }
+}
+
+#[test]
+fn test_move() {
+    let instrs = ["F10", "N3", "F7", "R90", "F11"].iter().map(|i| i.parse()).collect::<Result<Vec<Instr>>>().unwrap();
+
+    let mut ship = Ship {
+        pos: Point::default(),
+        waypoint: Point { x: 10, y: 1 }
+    };
+    for instr in instrs {
+        println!("ship: {:?} instr: {:?}", ship, instr);
+        ship.go(instr);
+    }
+
+    assert_eq!(ship.manhattan_distance(), 286);
+
 }
 
 fn main() -> Result<()> {
     let input = std::fs::read_to_string("input.txt")?;
     let instrs = input.lines().map(|l| l.parse()).collect::<Result<Vec<Instr>>>()?;
-    let mut ship = Ship::default();
+    let mut ship = Ship {
+        pos: Point::default(),
+        waypoint: Point { x: 10, y: 1 }
+    };
     for instr in instrs {
         println!("ship: {:?} instr: {:?}", ship, instr);
         ship.go(instr);
