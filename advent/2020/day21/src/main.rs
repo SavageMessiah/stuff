c25d6c552158932f3674bd7780565c559ac4514b
SavageMessiah <cardboard42@gmail.com>
2023-08-31 14:01:30 -0400

    advent 2020 day 21 part 2
    this one was a lot simpler than the last one, thankfully

--- advent/2020/day21/src/main.rs
+++ advent/2020/day21/src/main.rs
@@ -22,8 +22,13 @@ fn parse_foods(s: &str) -> Vec<Food> {
     s.lines().map(|l| l.parse()).collect::<Result<Vec<_>, _>>().unwrap()
 }
 
-fn no_allergen_ingredients(foods: &[Food]) -> HashSet<String> {
-    let mut allergen_food_counts = foods.iter().flat_map(|f| f.allergens.iter()).counts();
+struct Counts<'a> {
+    allergen_food_counts: HashMap<&'a String,usize>,
+    ingredient_allergen_counts: HashMap<&'a String,HashMap<&'a String, usize>>
+}
+
+fn allergen_counts(foods: &[Food]) -> Counts {
+    let allergen_food_counts = foods.iter().flat_map(|f| f.allergens.iter()).counts();
     let mut ingredient_allergen_counts = HashMap::new();
     for f in foods {
         for i in &f.ingredients {
@@ -33,21 +38,62 @@ fn no_allergen_ingredients(foods: &[Food]) -> HashSet<String> {
             }
         }
     }
+    Counts {
+        allergen_food_counts, ingredient_allergen_counts
+    }
+}
+
+fn partition_ingredients<'a>(counts: &Counts<'a>) -> (HashSet<&'a String>, HashSet<&'a String>) {
     let mut no_allergen_ingredients = HashSet::new();
-    'outer: for (i, allergen_counts) in ingredient_allergen_counts {
+    let mut allergen_ingredients = HashSet::new();
+    'outer: for (i, allergen_counts) in &counts.ingredient_allergen_counts {
         for (allergen, count) in allergen_counts {
-            println!("ingredient {} is in {} foods with {} which is in {} foods", i, count, &allergen, allergen_food_counts.get(&allergen).unwrap());
-            if count == *allergen_food_counts.get(&allergen).unwrap() {
+            println!("ingredient {} is in {} foods with {} which is in {} foods", i, count, &allergen, counts.allergen_food_counts.get(allergen).unwrap());
+            if count == counts.allergen_food_counts.get(allergen).unwrap() {
+                allergen_ingredients.insert(*i);
                 continue 'outer;
             }
         }
-        no_allergen_ingredients.insert(i.clone());
+        no_allergen_ingredients.insert(*i);
     }
 
-    no_allergen_ingredients
+    (allergen_ingredients, no_allergen_ingredients)
 }
 
-fn count_ingredients(foods: &[Food], ingredients: &HashSet<String>) -> usize {
+fn identify_allergens(allergens: &HashSet<&String>, counts: &Counts) -> HashMap<String, String> {
+    let mut identified_allergens = HashMap::new();
+    let mut unidentified_allergen_ingredients = allergens.clone();
+    while identified_allergens.len() < allergens.len() {
+        for ingredient in allergens {
+            // looping over allergens and excluding identified ones because we need to modify
+            // unidentified_allergen_ingredients
+            if !unidentified_allergen_ingredients.contains(ingredient) {
+                continue;
+            }
+            println!("ingredient: {}", ingredient);
+            let potential_allergens = counts.ingredient_allergen_counts.get(ingredient).unwrap().iter()
+                                                                       .filter_map(|(a, c)| {
+                                                                           if c == counts.allergen_food_counts.get(a).unwrap() &&
+                                                                              !identified_allergens.contains_key(*a) {
+                                                                                  Some(a)
+                                                                              } else {
+                                                                                  None
+                                                                              }
+                                                                       }).collect::<Vec<_>>();
+            println!("potential allergens: {:?}", potential_allergens);
+            if potential_allergens.len() == 1 {
+                let allergen = potential_allergens[0];
+                println!("identified {} as {}", ingredient, allergen);
+                identified_allergens.insert(allergen.to_string(), ingredient.to_string());
+                unidentified_allergen_ingredients.remove(ingredient);
+            }
+        }
+    }
+
+    identified_allergens
+}
+
+fn count_ingredients(foods: &[Food], ingredients: &HashSet<&String>) -> usize {
     foods.iter().flat_map(|f| f.ingredients.iter()).filter(|i| ingredients.contains(*i)).count()
 }
 
@@ -57,16 +103,22 @@ fn test_count_no_allergen_ingredients() {
 trh fvjkl sbzzf mxmxvkd (contains dairy)
 sqjhc fvjkl (contains soy)
 sqjhc mxmxvkd sbzzf (contains fish)");
-    let nai = no_allergen_ingredients(&foods);
-    assert_eq!(nai, HashSet::from(["kfcds".to_string(), "nhms".to_string(), "sbzzf".to_string(), "trh".to_string()]));
+    let counts = allergen_counts(&foods);
+    let (ai, nai) = partition_ingredients(&counts);
+
+    assert_eq!(ai, HashSet::from([&"sqjhc".to_string(), &"fvjkl".to_string(), &"mxmxvkd".to_string()]));
+    assert_eq!(nai, HashSet::from([&"kfcds".to_string(), &"nhms".to_string(), &"sbzzf".to_string(), &"trh".to_string()]));
     assert_eq!(count_ingredients(&foods, &nai), 5);
 }
 
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
     let foods = parse_foods(&input);
-    let nai = no_allergen_ingredients(&foods);
+    let counts = allergen_counts(&foods);
+    let (ai, nai) = partition_ingredients(&counts);
 
-    let answer = count_ingredients(&foods, &nai);
+    let identified = identify_allergens(&ai, &counts);
+    let answer = identified.iter().sorted_by_key(|kv| kv.0).map(|kv| kv.1).join(",");
+
     println!("answer: {}", answer);
 }
