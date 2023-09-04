76113ad744069a1b46461090801ca33907874b8b
SavageMessiah <cardboard42@gmail.com>
2023-09-04 18:51:31 -0400

    advent 2020 day 23 part 2: the vectoring
    converted to vector, runtime down to 178ms lmao

--- advent/2020/day23/src/main.rs
+++ advent/2020/day23/src/main.rs
@@ -1,44 +1,45 @@
-use std::collections::HashMap;
-
 struct Circle {
-    current: u64,
-    max: u64,
-    all: HashMap<u64, u64>,
-    remove_buf: [u64; 3]
+    current: usize,
+    max: usize,
+    all: Vec<usize>,
+    remove_buf: [usize; 3]
 }
 
-fn find_destination2(current: u64, max: u64, removed: &[u64; 3]) -> u64 {
+fn find_destination(current: usize, max: usize, removed: &[usize; 3]) -> usize {
     let mut dest = current - 1;
     while removed.contains(&dest) {
         dest = dest - 1;
     }
     if dest == 0 {
-        return find_destination2(max + 1, max, removed);
+        return find_destination(max + 1, max, removed);
 
     }
     dest
 }
 
 impl Circle {
-    fn new(contents: impl IntoIterator<Item = u64>) -> Circle {
-        let mut iter = contents.into_iter();
-        let current = iter.next().unwrap();
-        let mut all = HashMap::new();
+    fn new(contents: &[usize]) -> Circle {
+        let current = contents[0];
+        let mut all = vec![0usize; contents.len()];
         let mut last = current;
-        for n in iter {
-            all.insert(last, n);
-            last = n;
+        for n in contents.iter().skip(1) {
+            all[last - 1] = *n;
+            last = *n;
         }
-        all.insert(last, current);
-        let max = *all.keys().max().unwrap();
+        all[last - 1] = current;
+        let max = *all.iter().max().unwrap();
 
         Circle {
             all, current, max, remove_buf: [0; 3]
         }
     }
 
-    fn next(&self, n: u64) -> u64 {
-        *self.all.get(&n).unwrap()
+    fn next(&self, n: usize) -> usize {
+        self.all[n - 1]
+    }
+
+    fn set_next(&mut self, n: usize, next: usize) {
+        self.all[n - 1] = next;
     }
 
     fn make_move(&mut self) {
@@ -49,13 +50,13 @@ impl Circle {
             self.remove_buf[i] = next;
         }
         //slice them out of the ring
-        self.all.insert(self.current, self.next(next));
-        let dest = find_destination2(self.current, self.max, &self.remove_buf);
+        self.set_next(self.current, self.next(next));
+        let dest = find_destination(self.current, self.max, &self.remove_buf);
 
         //splice them back in
         let end = self.next(dest);
-        self.all.insert(dest, self.remove_buf[0]);
-        self.all.insert(self.remove_buf[2], end);
+        self.set_next(dest, self.remove_buf[0]);
+        self.set_next(self.remove_buf[2], end);
 
         self.current = self.next(self.current);
     }
@@ -76,28 +77,28 @@ impl Circle {
         s
     }
 
-    fn result_product(&self) -> u64 {
+    fn result_product(&self) -> usize {
         let first = self.next(1);
         let second = self.next(first);
         first * second
     }
 }
 
-fn parse_input(s: &str) -> Vec<u64> {
-    s.chars().map(|c| c.to_digit(10).unwrap() as u64 ).collect()
+fn parse_input(s: &str) -> Vec<usize> {
+    s.chars().map(|c| c.to_digit(10).unwrap() as usize ).collect()
 }
-
 
 #[test]
 fn test_make_moves() {
-    let mut circle = Circle::new(parse_input("389125467"));
+    let mut circle = Circle::new(&parse_input("389125467"));
     circle.make_moves(10);
     assert_eq!(circle.result_string(), "92658374");
 }
 
 fn main() {
-    let parsed = parse_input("253149867");
-    let mut circle = Circle::new(parsed.iter().copied().chain(10u64..=1000000));
+    let mut parsed = parse_input("253149867");
+    parsed.extend(10usize..=1000000);
+    let mut circle = Circle::new(&parsed);
     circle.make_moves(10000000);
     println!("{}", circle.result_product());
 }
