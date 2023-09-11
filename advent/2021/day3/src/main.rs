ff381bcc1fcd0504272a2946fd5d9b2d254d4fd9
SavageMessiah <cardboard42@gmail.com>
2023-09-11 13:02:35 -0400

    advent 2021 day 3 part 2

--- advent/2021/day3/src/main.rs
+++ advent/2021/day3/src/main.rs
@@ -1,27 +1,45 @@
-fn main() -> anyhow::Result<()> {
-    let input = std::fs::read_to_string("input.txt")?;
-    let mut counts = [[0; 2]; 12];
-    for l in input.lines() {
-        for (i, c) in l.chars().enumerate() {
-            let bit = if c == '1' { 1 } else { 0 };
-            counts[i][bit] += 1;
+use anyhow::anyhow;
+use itertools::Itertools;
+
+fn common_bit(strs: &[&str], i: usize) -> Option<char> {
+    let counts = strs.iter().map(|s| s.chars().nth(i).unwrap()).counts();
+    if counts[&'0'] == counts[&'1'] {
+        None
+    } else if counts[&'1'] > counts[&'0'] {
+        Some('1')
+    } else {
+        Some('0')
+    }
+}
+
+fn find_match<'a, F: Fn(Option<char>) -> char>(strs: &[&'a str], f: F) -> Option<&'a str> {
+    let mut prefix = "".to_string();
+    let mut matches = strs.to_vec();
+    for i in 0.. {
+        prefix.push(f(common_bit(&matches, i)));
+
+        matches = strs.iter().filter(|l| l.starts_with(&prefix)).copied().collect::<Vec<&str>>();
+        println!("{} {}", prefix, matches.len());
+        if matches.len() == 1 {
+            return Some(matches[0])
         }
     }
-    let mut gamma = 0;
-    let mut epsilon = 0;
+    None
+}
 
-    for bit in counts {
-        let (most, least) = if bit[0] < bit[1] {
-            (1, 0)
-        } else {
-            (0, 1)
-        };
-        println!("{:?} {} {}", bit, most, least);
-        gamma = (gamma << 1) + most;
-        epsilon = (epsilon << 1) + least;
-    }
+fn main() -> anyhow::Result<()> {
+    let input = std::fs::read_to_string("input.txt")?;
 
-    let answer = gamma * epsilon;
+    let lines = input.lines().collect::<Vec<_>>();
+    let oxygen = u32::from_str_radix(find_match(&lines, |b| b.unwrap_or('1')).ok_or(anyhow!("wut"))?, 2)?;
+    let scrubber = u32::from_str_radix(find_match(&lines, |b| {
+        match b {
+            Some('0') => '1',
+            _ => '0'
+        }
+    }).ok_or(anyhow!("wut"))?, 2)?;
+
+    let answer = oxygen * scrubber;
 
     println!("answer {}", answer);
 
