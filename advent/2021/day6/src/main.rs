251239b9bcaa7265f12acbd4804a7a2120144625
SavageMessiah <cardboard42@gmail.com>
2023-09-14 14:29:35 -0400

    advent 2021 day 6 part 2

--- advent/2021/day6/src/main.rs
+++ advent/2021/day6/src/main.rs
@@ -1,40 +1,53 @@
 use std::{num::ParseIntError, iter::repeat};
 
-fn parse_input(s: &str) -> Result<Vec<u8>, ParseIntError> {
-    s.split(',').map(|n| n.trim_end().parse() ).collect()
-}
+use itertools::Itertools;
 
-fn step(fish: &mut Vec<u8>) {
-    let mut spawned = 0;
-    for f in &mut *fish {
-        if *f == 0 {
-            *f = 6;
-            spawned += 1;
-        } else {
-            *f -= 1;
-        }
+
+type Fish = [u64; 9];
+
+fn parse_input(s: &str) -> Result<Fish, ParseIntError> {
+    let all_fish = s.split(',').map(|n| n.trim_end().parse() ).collect::<Result<Vec<usize>, _>>()?;
+    let counts = all_fish.iter().counts();
+    let mut fish = [0u64; 9];
+    for i in 0..fish.len() {
+        fish[i] = *counts.get(&i).unwrap_or(&0usize) as u64;
     }
-    fish.reserve(spawned);
-    fish.extend(repeat(8).take(spawned));
+    Ok(fish)
 }
 
-fn run(initial_fish: &Vec<u8>, steps: u32) -> usize {
+fn step(fish: &Fish) -> Fish {
+    let mut next = [0u64; 9];
+    next[8] = fish[0];
+    for i in 1..=8 {
+        next[i - 1] = fish[i];
+    }
+    next[6] += fish[0];
+    next
+}
+
+fn count_fish(fish: &Fish) -> u64 {
+    fish.iter().sum()
+}
+
+fn run(initial_fish: &Fish, steps: u32) -> u64 {
     let mut fish = initial_fish.clone();
-    println!("initial fish: {}", fish.len());
+    println!("initial fish: {} {:?}", count_fish(&fish), fish);
     for i in 1..=steps {
-        step(&mut fish);
-        println!("after {} days: {}", i, fish.len());
+        fish = step(&fish);
+        println!("after {} days: {} {:?}", i, count_fish(&fish), fish);
     }
 
-    fish.len()
+    count_fish(&fish)
 }
 
 #[test]
 fn test_score() {
     let fish = parse_input("3,4,3,1,2").unwrap();
+    println!("{:?}", fish);
 
     assert_eq!(run(&fish, 18), 26);
     assert_eq!(run(&fish, 80), 5934);
+    assert_eq!(run(&fish, 256), 26984457539);
 }
 
 
@@ -42,7 +55,7 @@ fn main() -> anyhow::Result<()> {
     let input = std::fs::read_to_string("input.txt")?;
     let fish = parse_input(&input)?;
 
-    println!("{}", run(&fish, 80));
+    println!("{}", run(&fish, 256));
 
     Ok(())
 }
