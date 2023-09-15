dff53aa620e6e1845579df6943568250d687f57b
SavageMessiah <cardboard42@gmail.com>
2023-09-15 17:35:24 -0400

    advent 2021 day 7 part 2

--- advent/2021/day7/src/main.rs
+++ advent/2021/day7/src/main.rs
@@ -1,43 +1,42 @@
 use std::num::ParseIntError;
 
+use itertools::{MinMaxResult, Itertools};
+
 fn parse_input(s: &str) -> Result<Vec<i32>, ParseIntError> {
     s.split(',').map(|n| n.trim_end().parse() ).collect()
 }
 
-fn fuel_usage(crabs: &[i32], target: i32) -> i32 {
-    crabs.iter().map(|c| (c - target).abs() ).sum()
+fn triangle(mut n: i32) -> i32 {
+    let mut t = n;
+    while n != 0 {
+        n -= 1;
+        t += n;
+    }
+    t
 }
 
-fn median_int(ints: &[i32]) -> i32 {
-    let mut ints = ints.to_owned();
-    ints.sort();
-    let mid = ints.len() / 2;
-
-    if ints.len() % 2 == 0 {
-        (ints[mid - 1] + ints[mid]) / 2
-    } else {
-        ints[mid]
-    }
+fn fuel_usage(crabs: &[i32], target: i32) -> i32 {
+    crabs.iter().map(|c| triangle((c - target).abs()) ).sum()
 }
 
 fn optimal_position(crabs: &[i32]) -> i32 {
-    median_int(crabs)
+    let MinMaxResult::MinMax(min, max) = crabs.iter().minmax() else { unreachable!() };
+    (*min..=*max).min_by_key(|target| fuel_usage(crabs, *target) ).unwrap()
 }
 
 #[test]
 fn test_fuel_usage() {
     let crabs = vec![16,1,2,0,4,2,7,1,2,14];
 
-    assert_eq!(fuel_usage(&crabs, 1), 41);
-    assert_eq!(fuel_usage(&crabs, 2), 37);
-    assert_eq!(fuel_usage(&crabs, 3), 39);
+    assert_eq!(fuel_usage(&crabs, 5), 168);
+    assert_eq!(fuel_usage(&crabs, 2), 206);
 }
 
 #[test]
 fn test_optimal_position() {
     let crabs = vec![16,1,2,0,4,2,7,1,2,14];
 
-    assert_eq!(optimal_position(&crabs), 2);
+    assert_eq!(optimal_position(&crabs), 5);
 }
 
 fn main() -> anyhow::Result<()> {
