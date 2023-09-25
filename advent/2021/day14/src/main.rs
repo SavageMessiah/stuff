e4351bd9c3ae0e6fdfc2eb4fb2cfde796e92943f
SavageMessiah <cardboard42@gmail.com>
2023-09-25 13:25:33 -0400

    advent 2021 day 14 part 2

--- advent/2021/day14/src/main.rs
+++ advent/2021/day14/src/main.rs
@@ -1,10 +1,45 @@
-use std::collections::HashSet;
+use std::{collections::HashMap, str::FromStr};
 
 use anyhow::anyhow;
 use itertools::{Itertools, MinMaxResult};
 
-type Polymer = String;
-type Rule = ((char, char), char);
+type Pair = (char, char);
+
+#[derive(Debug)]
+struct Polymer {
+    pairs: HashMap<Pair, usize>,
+    counts: HashMap<char, usize>
+}
+
+impl FromStr for Polymer {
+    type Err = anyhow::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let pairs = s.chars().tuple_windows().counts();
+        let counts = s.chars().counts();
+
+        Ok(Polymer {pairs, counts})
+    }
+}
+
+impl Polymer {
+    fn insert_pair(&mut self, pair: Pair, n: usize) {
+        *self.pairs.entry(pair).or_insert(0) += n
+    }
+    fn insert_split(&mut self, pair: Pair, n: usize, insert: char) {
+        self.insert_pair((pair.0, insert), n);
+        *self.counts.entry(insert).or_insert(0) += n;
+        self.insert_pair((insert, pair.1), n);
+    }
+
+    fn score(&self) -> usize {
+        match self.counts.values().minmax() {
+            MinMaxResult::MinMax(min, max) => max - min,
+            _ => 0,
+        }
+    }
+}
+
+type Rule = (Pair, char);
 
 fn parse_input(input: &str) -> anyhow::Result<(Polymer, Vec<Rule>)> {
     let (template, rules) = input.split_once("\n\n").ok_or(anyhow!("missing rule section"))?;
@@ -12,37 +47,32 @@ fn parse_input(input: &str) -> anyhow::Result<(Polymer, Vec<Rule>)> {
         let (pair, insert) = l.split_once(" -> ").ok_or(anyhow!("bad rule {}", l))?;
         Ok((pair.chars().tuple_windows().nth(0).unwrap(), insert.chars().nth(0).ok_or(anyhow!("missing insert char"))?))
     }).collect::<anyhow::Result<Vec<_>>>()?;
-    Ok((template.to_string(), rules))
+    Ok((template.parse()?, rules))
 }
 
+
 fn apply(poly: &Polymer, rules: &[Rule]) -> Polymer {
-    let mut new = poly[0..1].to_string();
-    for pair in poly.chars().tuple_windows::<(_, _)>() {
-        for (rule, insert) in rules {
-            if *rule == pair {
-                new.push(*insert);
-                break;
-            }
+    let mut new = Polymer {
+        pairs: HashMap::new(),
+        counts: poly.counts.clone()
+    };
+
+    for (pair, count) in &poly.pairs {
+        let rule = rules.iter().find(|(rule, _)| rule == pair);
+        match rule {
+            Some((_, insert)) => new.insert_split(*pair, *count, *insert),
+            None => new.insert_pair(*pair, *count)
         }
-        new.push(pair.1);
     }
     new
 }
 
-fn apply_n(poly: &Polymer, rules: &[Rule], n: usize) -> Polymer {
-    let mut poly = poly.clone();
+fn apply_n(mut poly: Polymer, rules: &[Rule], n: usize) -> Polymer {
     for i in 1..=n {
         println!("applying rules step {}", i);
         poly = apply(&poly, rules);
     }
     poly
-}
-
-fn score(poly: &Polymer) -> usize {
-    match poly.chars().counts().values().minmax() {
-        MinMaxResult::MinMax(min, max) => max - min,
-        _ => 0,
-    }
 }
 
 #[test]
@@ -66,20 +96,22 @@ BC -> B
 CC -> N
 CN -> C").unwrap();
 
-    assert_eq!(apply(&polymer, &rules), "NCNBCHB");
+    println!("{:?}", polymer);
 
-    polymer = apply_n(&polymer, &rules, 10);
+    println!("{:?}", apply(&polymer, &rules));
 
-    assert_eq!(score(&polymer), 1588);
+    polymer = apply_n(polymer, &rules, 10);
+
+    assert_eq!(polymer.score(), 1588);
 }
 
 fn main() -> anyhow::Result<()> {
     let input = std::fs::read_to_string("input.txt")?;
     let (mut polymer, rules) = parse_input(&input)?;
 
-    polymer = apply_n(&polymer, &rules, 10);
+    polymer = apply_n(polymer, &rules, 40);
 
-    println!("{}", score(&polymer));
+    println!("{}", polymer.score());
 
     Ok(())
 }
